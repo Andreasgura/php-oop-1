fb61d8b58dc64829fdd6d42a14c9ee5938d991c3
Andreasgura <andre-56@hotmail.it>
2024-05-07 16:35:32 +0200

    cards printed (no img)

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<?php 
+<?php
 include __DIR__ . "/./Views/head.php";
-include __DIR__ ."/Controllers/printmovies.php";
-printMovies();
+include __DIR__ . "/Controllers/printmovies.php";
+$movieList = printMovies();
 
 ?>
 
 <main class="container">
-    
+    <div class="row">
+        <?php foreach ($movieList as $movie) { ?>
+            <div class="col-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $movie->original_title ?></h5>
+                        <p class="card-text"><?= $movie->overview ?></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 </main>
 
 
